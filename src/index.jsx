0ed959ac82b24e5297f5b959a8eaa305b019140d
Абдуллаев Эмиль Расим <Abdu_xj50@itstep.edu.az>
2023-11-22 18:12:40 +0400

    added slice for tasks and store in redux folder

---
+++ src/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider, } from "react-router-dom";
+import { Provider } from 'react-redux';
+import App from './App';
+import store from './redux/slice';
+const container = document.getElementById('root');
+
+if (container === null) throw new Error('You don\'t have root element');
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+	},
+]);
+
+const root = ReactDOM.createRoot(container);
+
+root.render(
+	<Provider store={store}>
+		<RouterProvider router={router} />
+	</Provider>
+);
---
+++ src/redux/slices/tasksSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const tasksSlice = createSlice({
+    name: 'tasks',
+    initialState: JSON.parse(localStorage.getItem('tasks')) ?? [],
+    reducers: {
+        addTask: (state, action) => {
+            state.push(action.payload);
+            localStorage.setItem('tasks', JSON.stringify(state));
+        },
+        deleteTask: (state, action) => {
+            const taskIndex = state.findIndex(task => task.id === action.payload);
+
+            state.splice(taskIndex, 1);
+            localStorage.setItem('tasks', JSON.stringify(state));
+        },
+        updateTask: (state, action) => {
+            const {id, title, description} = action.payload;
+            const taskIndex = state.findIndex(task => task.id === id);
+            state[taskIndex].title = title;
+            state[taskIndex].description = description;
+            localStorage.setItem('tasks', JSON.stringify(state));
+        },
+    }
+});
+
+export const { addTask, deleteTask, updateTask } = tasksSlice.actions;
+
+export default tasksSlice.reducer;

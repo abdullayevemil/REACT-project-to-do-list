03bef5feaa511500a98098c8a4fde102663d5bfe
Абдуллаев Эмиль Расим <Abdu_xj50@itstep.edu.az>
2023-11-22 18:15:42 +0400

    added Task component

---
+++ src/components/Task/Task.jsx
@@ -0,0 +1,25 @@
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { deleteTask } from "../../../redux/slices/tasksSlice";
+
+function Task({ taskData }) {
+    const dispatch = useDispatch();
+    return (
+        <div>
+            <input type="checkbox" />
+            <Link to={`/tasks/${taskData.id}`}>{
+                <i>{taskData.title}</i>
+            }</Link>
+            <Link to={`/tasks/${taskData.id}/edit`}>
+                <button type="submit">Edit</button>
+            </Link>
+            <Link to={'/'}>
+                <button type="button" onClick={() => {
+                    dispatch(deleteTask(taskData.id));
+                }}>Delete</button>
+            </Link>
+        </div>
+    )
+}
+
+export default Task;
